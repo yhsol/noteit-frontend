393cff9b3dba3366f9d7f45a3a797eff3e0555dc
yhsol <yhsol1592@gmail.com>
2019-08-20 21:36:14 +0900

    add isLoggedIn boolean type

--- src/Apollo/LocalState.tsx
+++ src/Apollo/LocalState.tsx
@@ -5,6 +5,7 @@ export const defaults = {
 export const resolvers = {
   Mutation: {
     //   context 로 부터 token, cache 등을 가져와서 로그인에 확인.
+    // apollo 에서 type 을 지정할 때 interface 를 지정할 방법 찾아야됨.
     logUserIn: (
       _: string,
       { token }: { token: string },
--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -4,16 +4,35 @@ import Theme from "../Styles/Theme";
 import GlobalStyles from "../Styles/GlobalStyles";
 import { BrowserRouter as Router } from "react-router-dom";
 import RouterComponent from "./RouterComponent";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
 
 interface IAppProps {}
 
+// interface LogInProps {
+//   isLoggedIn: boolean;
+// }
+// useQuery 에서 interface 사용 방법 찾기.
+// const {data: { isLoggedIn }} = useQuery(LogInQuery) 같은 식으로 데이터를 꺼낼 때 type 을 지정하는 방법 찾기.
+// https://www.apollographql.com/docs/react/recipes/static-typing/ 참조.
+
+const LogInQuery = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
 const App: React.FunctionComponent<IAppProps> = () => {
+  const { data } = useQuery(LogInQuery);
+  const { isLoggedIn }: { isLoggedIn: boolean } = data;
+  console.log(isLoggedIn);
+
   return (
     <ThemeProvider theme={Theme}>
       <>
         <GlobalStyles />
         <Router>
-          <RouterComponent isLoggedIn={true} />
+          <RouterComponent isLoggedIn={isLoggedIn} />
         </Router>
       </>
     </ThemeProvider>

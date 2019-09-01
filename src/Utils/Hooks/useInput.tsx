f08898b1775f72b1eb5ef56527829ea2144005e7
yhsol <yhsol1592@gmail.com>
2019-09-01 20:23:36 +0900

    add Type of useInput

--- src/Utils/Hooks/useInput.tsx
+++ src/Utils/Hooks/useInput.tsx
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 
 interface IuseInputProps {}
 
+type TypeUseInput = {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+};
+
 // React.FunctionComponent 로는 작동안됨.
 // const { target: { value } } = e; 같은 형식도 타입 요소를 잘 지정하면 잘 작동함.
 
@@ -9,7 +15,7 @@ interface IuseInputProps {}
 // 심지어 해당 컴포넌트를 사용할 때 역시 hooks 뒤에 따로 타입을 지정해주지 않아도 다 알고 있음.
 // 이럴때는 안써도 되는건가?
 
-const useInput = (defaultValue: string) => {
+const useInput = (defaultValue: string): TypeUseInput => {
   const [value, setValue] = useState<string>(defaultValue);
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {

dd4f19a92b36ce8162424d09c3e52ebb3b832374
yhsol <yhsol1592@gmail.com>
2019-09-10 20:37:01 +0900

    send seeFullPost to PostPresenter

--- src/Routes/Post/PostContaier.tsx
+++ src/Routes/Post/PostContaier.tsx
@@ -3,22 +3,34 @@ import { useQuery } from "react-apollo-hooks";
 import { POST_QUERY } from "./PostQuery";
 import { withRouter, RouteComponentProps } from "react-router";
 import Loader from "../../Components/Loader";
+import PostPresenter from "./PostPresenter";
 
 type IPostContaienr = RouteComponentProps<{ id: string }, any, any>;
 
 const PostContainer: React.FunctionComponent<IPostContaienr> = ({
   match: {
-    params: { id }
+    params: { id: postId }
   }
 }) => {
-  const { data, loading } = useQuery(POST_QUERY, { variables: { id } });
-  console.log(data, loading);
-
+  const { data, loading } = useQuery(POST_QUERY, { variables: { id: postId } });
+  const post = data.seeFullPost;
   return (
     <>
       {loading && <Loader />}
-      <div>postId: {id}</div>
-      {!loading && data && data.seeFullPost && <div>seeFullPost!</div>}
+      {!loading && data && post && (
+        <PostPresenter
+          id={post.id}
+          user={post.user}
+          title={post.title}
+          text={post.text}
+          tags={post.tags}
+          files={post.files}
+          isLiked={post.isLiked}
+          likeCount={post.likeCount}
+          commentCount={post.commentCount}
+          createdAt={post.createdAt}
+        />
+      )}
     </>
   );
 };
--- src/Routes/Post/PostPresenter.tsx
+++ src/Routes/Post/PostPresenter.tsx
@@ -1,9 +1,59 @@
 import * as React from "react";
 
-interface IPostPresenter {}
+interface IUserProps {
+  id: string;
+  username: string;
+  avatar: string;
+}
 
-const PostPresenter: React.FunctionComponent<IPostPresenter> = () => {
-  return <div>post container</div>;
+interface ITagsProps {
+  id: string;
+  text: string;
+}
+
+interface IFilesProps {
+  id: string;
+  url: string;
+}
+
+interface IPostPresenter {
+  id: string;
+  user: IUserProps;
+  title: string;
+  text: string;
+  tags: ITagsProps;
+  files: Array<IFilesProps>;
+  isLiked: boolean;
+  likeCount: number;
+  commentCount: number;
+  createdAt: number;
+}
+
+const PostPresenter: React.FunctionComponent<IPostPresenter> = ({
+  id,
+  user,
+  title,
+  text,
+  tags,
+  files,
+  isLiked,
+  likeCount,
+  commentCount,
+  createdAt
+}) => {
+  return (
+    <>
+      <div>{title}</div>
+      <div>{user.username}</div>
+      <div>{text}</div>
+      <div>{tags && "tags need map"}</div>
+      <div>{files && "files need map"}</div>
+      <div>{isLiked ? "is liked" : "is no liked"}</div>
+      <div>{likeCount}</div>
+      <div>{commentCount}</div>
+      <div>{createdAt}</div>
+    </>
+  );
 };
 
 export default PostPresenter;

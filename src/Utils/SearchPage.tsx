0d33bfd47cc24873c3fb8ce047077bb737cea55e
yhsol <yhsol1592@gmail.com>
2019-10-01 20:15:09 +0900

    add SearchPage for smallFoterSearchIcon

---
+++ src/Utils/SearchPage.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import useInput from "./Hooks/useInput";
+import { withRouter, RouteComponentProps } from "react-router";
+import Input from "./Input";
+
+// TODO: search 컴포넌트에서 input 부분은 작은 화면에서만 보여줘야 됨.
+
+type ISearchProps = RouteComponentProps;
+
+const SearchPage: React.FunctionComponent<ISearchProps> = props => {
+  const search = useInput("");
+  const onSearchSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    props.history.push(`/search?term=${search.value}`);
+  };
+  return (
+    <div>
+      <form onSubmit={onSearchSubmit}>
+        <Input
+          value={search.value}
+          onChange={search.onChange}
+          placeholder="search.."
+          icon={true}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default withRouter(SearchPage);

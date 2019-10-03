c837596c12bb471aa9b132f72ddda7c7165720f9
yhsol <yhsol1592@gmail.com>
2019-10-03 17:42:25 +0900

    design search field

--- src/Utils/SearchPage.tsx
+++ src/Utils/SearchPage.tsx
@@ -2,6 +2,33 @@ import React from "react";
 import useInput from "./Hooks/useInput";
 import { withRouter, RouteComponentProps } from "react-router";
 import Input from "./Input";
+import styled from "styled-components";
+import { SearchIcon } from "./Icons";
+
+const Form = styled.div`
+  background-color: ${props => props.theme.backgroundColor};
+  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+  padding: 0px 0.5rem;
+  width: 80%;
+  margin: 2rem auto;
+  height: 1.7rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const SInput = styled.input`
+  border: none;
+  font-size: 20px;
+  background-color: inherit;
+  width: 100%;
+`;
+
+const SButton = styled.button`
+  border: none;
+  background-color: inherit;
+  cursor: pointer;
+`;
 
 // TODO: search 컴포넌트에서 input 부분은 작은 화면에서만 보여줘야 됨.
 
@@ -15,14 +42,19 @@ const SearchPage: React.FunctionComponent<ISearchProps> = props => {
   };
   return (
     <div>
-      <form onSubmit={onSearchSubmit}>
-        <Input
+      <Form>
+        <SInput
           value={search.value}
           onChange={search.onChange}
-          placeholder="search.."
-          icon={true}
+          placeholder={"search"}
+          required={true}
+          type={"text"}
+          className={"className"}
         />
-      </form>
+        <SButton>
+          <SearchIcon size={17} />
+        </SButton>
+      </Form>
     </div>
   );
 };

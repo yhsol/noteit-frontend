376734c9d8d91fb647195cda2366d872e9a89510
yhsol <yhsol1592@gmail.com>
2019-10-11 14:14:21 +0900

    url

--- src/Apollo/Client.tsx
+++ src/Apollo/Client.tsx
@@ -6,7 +6,10 @@ import { defaults, resolvers } from "./LocalState";
 // });
 
 export const Client = new ApolloClient({
-  uri: "http://localhost:4010",
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4010"
+      : "https://noteitblog-server-2bf8f48232.herokuapp.com/noteit/prod",
   clientState: {
     defaults,
     resolvers
